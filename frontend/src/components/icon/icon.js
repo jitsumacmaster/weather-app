fb2172b09a18f8cbf50771d7a5febac1689f8f88
jitsumacmaster <jitsu.macmaster@gmail.com>
2021-03-23 20:49:47 -0700

    Add alt prop to Icon Component

--- frontend/src/components/icon/icon.js
+++ frontend/src/components/icon/icon.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Icon extends React.Component {
   render() {
     return (
-        <img src={`http://www.openweathermap.org/img/wn/${this.props.icon}@2x.png`}/>
+        <img src={`http://www.openweathermap.org/img/wn/${this.props.icon}@2x.png`} alt="icon"/>
         );
       }
 }
